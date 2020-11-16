a7b1180ae6ccec07b49586aee4781309255307b9
Ricardo Medina <rmedina0531@gmail.com>
2020-11-16 01:23:14 -0800

    added gitignore, modified data to return by state

--- bayesianGraphUnitTest.py
+++ bayesianGraphUnitTest.py
@@ -1,5 +1,83 @@
 import unittest
-from bayesian_network_graph import Bayesian_graph
+from pomegranate import *
+from bayesian_network_graph import Bayesian_graph, State_Data
+
+def generate_test_network():
+    guest_door = DiscreteDistribution({'A':1.0/3, 'B':1.0/3, 'C':1.0/3})
+    prize_door = DiscreteDistribution({'A':1.0/3, 'B':1.0/3, 'C':1.0/3})
+
+    monty_door = ConditionalProbabilityTable(
+        [[ 'A', 'A', 'A', 0.0],
+        [ 'A', 'A', 'B', 0.5],
+        [ 'A', 'A', 'C', 0.5],
+        [ 'A', 'B', 'A', 0.0],
+        [ 'A', 'B', 'B', 0.0],
+        [ 'A', 'B', 'C', 1.0],
+        [ 'A', 'C', 'A', 0.0],
+        [ 'A', 'C', 'B', 1.0],
+        [ 'A', 'C', 'C', 0.0],
+        [ 'B', 'A', 'A', 0.0],
+        [ 'B', 'A', 'B', 0.0],
+        [ 'B', 'A', 'C', 1.0],
+        [ 'B', 'B', 'A', 0.5],
+        [ 'B', 'B', 'B', 0.0],
+        [ 'B', 'B', 'C', 0.5],
+        [ 'B', 'C', 'A', 1.0],
+        [ 'B', 'C', 'B', 0.0],
+        [ 'B', 'C', 'C', 0.0],
+        [ 'C', 'A', 'A', 0.0],
+        [ 'C', 'A', 'B', 1.0],
+        [ 'C', 'A', 'C', 0.0],
+        [ 'C', 'B', 'A', 1.0],
+        [ 'C', 'B', 'B', 0.0],
+        [ 'C', 'B', 'C', 0.0],
+        [ 'C', 'C', 'A', 0.5],
+        [ 'C', 'C', 'B', 0.5],
+        [ 'C', 'C', 'C', 0.0]], [guest_door, prize_door])
+        
+    #s1 = State( guest_door, name='guest')
+    #s2 = State( prize_door, name='prize')
+    #s3 = State( monty_door, name='monty')
+    
+    state1 = State_Data(State( guest_door, name='guest'), ['A', 'B', 'C'])
+    state2 = State_Data(State( prize_door, name='prize'), ['A', 'B', 'C'])
+    state3 = State_Data(State( monty_door, name='monty'), ['A', 'B', 'C'])
+
+    network = Bayesian_graph( "Monty Hall Problem" )
+    network.add_states(state1, state2, state3)
+    network.add_edge(state1, state3)
+    network.add_edge(state2, state3)
+    network.bake()
+    return network
 
 class TestGraphMethods(unittest.TestCase):
     
+    ##check to see if new class is working
+    def test_extended_bayesian_graph(self):
+        network = generate_test_network()
+        self.assertEqual(len(network.state_data),  3) 
+    
+    def test_get_data_length(self):
+        network = generate_test_network()
+        beliefs = network.predict_proba({'guest': 'A'})
+        data = network.get_data(beliefs)
+        self.assertEqual(len(data), 2)
+    
+    def test_get_data_length2(self):
+        network = generate_test_network()
+        beliefs = network.predict_proba({'guest': 'A'})
+        data = network.get_data(beliefs)
+        #print(data)
+        self.assertEqual(data[0][0], 'prize')
+        self.assertEqual(data[0][1], 'monty')
+        
+        
+    def test_graph(self):
+        network = generate_test_network()
+        beliefs = network.predict_proba({'guest': 'A'})
+        print(network.get_data(beliefs))
+        network.show_data(beliefs)
+        
+    
+if __name__== '__main__':
+    unittest.main()
--- bayesian_network_graph.py
+++ bayesian_network_graph.py
@@ -1,35 +1,42 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from pomegranate import *
 class State_Data:
-    def __init__(state, choices):
+    def __init__(self, state, choices):
         self.name = state.name
         self.choices = choices
         self.state = state
-    def get_choices():
+    def get_choices(self):
         return self.choices
     
-    def get_state():
+    def get_state(self):
         return self.state
         
-    def get_name():
+    def get_name(self):
         return self.name
 
 class Bayesian_graph(BayesianNetwork):
-    def __init__(title):
-        self.state_data = []
+    def __init__(self, title):
+        self.state_data = {}
         super().__init__(title)
         
-    def add_states(*args):
+    def add_states(self, *args):
         states = []
         for x in args:
-            self.state_data.append(x)
+            self.state_data[x.get_name()] = x
             states.append(x.get_state())
         super().add_states(*states)
         
-    def get_data(network, beliefs):
+    def add_edge(self, *args):
+        states = []
+        for x in args:
+            states.append(x.get_state())
+        super().add_edge(*states)
+        
+    def get_data(self, beliefs):
         labels = []
         data = []
-        for state, belief in zip(network.states, beliefs):
+        for state, belief in zip(self.states, beliefs):
             if type(belief) != type("srt"):
                 labels.append(state.name)
                 label_data = []
@@ -37,84 +44,35 @@ class Bayesian_graph(BayesianNetwork):
                     label_data.append(belief.parameters[0][p])
                 data.append(label_data)
         
-        data = list(zip(*data))
-        data2 = []
-        for d in data:
-            data2.append(list(d))
-            
-        return [labels, data2]
-        
-    def show_data
-        
-        bars = []
-        ##first bar
-        bars.append(np.arrange(len(labels))
-        ##add the rest if they exists
-        for i in range(1, len(data)):
-            bars.append([j+w for j in bars[i-1])
-        
-        
-        x = np.arange(len(labels))
-        width = 0.25
-        
-        fig, ax = plt.subplots()
-        rects = []
-        for i in labels:
-            ax.bar(x - width, data[0], width, label=temp[0])
-        
-        
-    #def show_data(network, beliefs):
-        #labels = []
-        #data = []
-        #for state, belief in zip(network.states, beliefs):
-            #if type(belief) != type("srt"):
-                #labels.append(state.name)
-                #label_data = []
-                #for p in belief.parameters[0]:
-                    #label_data.append(belief.parameters[0][p])
-                #data.append(label_data)
-        
         #data = list(zip(*data))
         #data2 = []
         #for d in data:
             #data2.append(list(d))
-        
-        
-        #x = np.arange(len(labels))
-        #width = 0.25
-        
-        #fig, ax = plt.subplots()
-        #rects = []
-        #for i in labels:
-            #ax.bar(x - width, data[0], width, label=temp[0])
-        
-        #rects1 = ax.bar(x - width, data[0], width, label=temp[0])
-        #rects2 = ax.bar(x, data[1], width, label=temp[1])
-        #rects3 = ax.bar(x + width, data[2], width, label=temp[2])
-        
-        #rects = []
-        #for i in range(len(temp)):
-            #rects.append(ax.bar(x - width/2, data[i], width, label=temp[i]))
             
-        #ax.set_ylabel('Scores')
-        #ax.set_title('Scores by Door Probabilities')
-        #ax.set_xticks(x)
-        #ax.set_xticklabels(labels)
-        #ax.legend()
+        return [labels, data]
+        
+    #def show_data(self, beliefs):
+        #data = self.get_data(beliefs)
+        #labels = data[0]
+        #elements = data[1]
+        
+        #w = 0.2
+        #bars = []
+        #first bar
+        #bars.append(np.arange(len(labels)))
+        #add the rest if they exists
+        #for i in range(1, len(data)):
+            #bars.append([j+w for j in bars[i-1]])
             
-        #def autolabel(rects):
-            #"""Attach a text label above each bar in *rects*, displaying its height."""
-            #for rect in rects:
-                #height = rect.get_height()
-                #ax.annotate('{}'.format(height),
-                            #xy=(rect.get_x() + rect.get_width() / 2, height),
-                            #xytext=(0, 3),  # 3 points vertical offset
-                            #textcoords="offset points",
-                            #ha='center', va='bottom')
+        #for i in range(len(bars)):
             
-        #for rect in rects:
-            #autolabel(rect)
-            
-        #fig.tight_layout()
+            #plt.bar(bars[i], elements[i], w, label=self.state_data[labels[i]].get_choices())
+        
+        #plt.xlabel("label1")
+        #plt.ylabel("label2")
+        #plt.title("Title")
+        #plt.legend()
+        #plt.xticks(bars[0], labels)
         #plt.show()
+
     
