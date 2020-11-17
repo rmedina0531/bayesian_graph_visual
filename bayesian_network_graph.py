bcd12799b49b6247cb9dae82a18c9aaabe92289d
Ricardo Medina <rmedina0531@gmail.com>
2020-11-16 20:03:04 -0800

    added comments to graph.py

--- bayesian_network_graph.py
+++ bayesian_network_graph.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pomegranate import *
+
+##state cointainer used for storing the possible choices of the state
+#Used for graphing purposes
+#takes a state and a list of choices for the state
 class State_Data:
     def __init__(self, state, choices):
         self.name = state.name
@@ -15,11 +19,18 @@ class State_Data:
     def get_name(self):
         return self.name
 
+#extends the BayesianNetwork class by adding a list of references to the new
+#state data class, for use in graphing
 class Bayesian_graph(BayesianNetwork):
     def __init__(self, title):
         self.state_data = {}
         super().__init__(title)
         
+        
+    #overrides the add_states method from the superclass
+    #adds the state_Data objects to the dictionary in the class
+    #while stripping the state object inside them to pass it to 
+    #the super add_state method
     def add_states(self, *args):
         states = []
         for x in args:
@@ -27,12 +38,16 @@ class Bayesian_graph(BayesianNetwork):
             states.append(x.get_state())
         super().add_states(*states)
         
+    #strips the state from insied the state data class to pass
+    #to the super add_edge method from the superclass
     def add_edge(self, *args):
         states = []
         for x in args:
             states.append(x.get_state())
         super().add_edge(*states)
         
+    #returns the labels of the belief states as well as the percentages of each
+    #possible choices
     def get_data(self, beliefs):
         labels = []
         data = []
@@ -51,6 +66,11 @@ class Bayesian_graph(BayesianNetwork):
             
         return [labels, data]
         
+        
+    #uses the netowrk and the belifs calculated to project a pie chart
+    #of all the states not being ovserved and the percentages of their
+    # possibilities
+    #Dynamically generated to function with any Bayesian network
     def show_data(self, beliefs):
         data = self.get_data(beliefs)
         labels = data[0]
