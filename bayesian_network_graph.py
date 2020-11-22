2f4f371133c9b45b1f44116bb8480fa37a922ec9
Ricardo Medina <rmedina0531@gmail.com>
2020-11-22 01:44:52 -0800

    Added description of dependencies

--- bayesian_network_graph.py
+++ bayesian_network_graph.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pomegranate import *
 
+#dependencies
+#cython 0.29.21
+#pomegranate 0.13.2
+
+
 ##state cointainer used for storing the possible choices of the state
 #Used for graphing purposes
 #takes a state and a list of choices for the state
